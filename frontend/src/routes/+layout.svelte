fbaec7080014c1f85f4b83d55b552f28add30160
Derk Blom <74244634+Derk-B@users.noreply.github.com>
2023-10-14 23:55:11 +0200

    Added icon library to website

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
 	import '../app.postcss';
 	import { AppShell } from '@skeletonlabs/skeleton';
+	import Header from './components/header.svelte';
 </script>
 
 <AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
-	<svelte:fragment slot="header">Header</svelte:fragment>
+	<svelte:fragment slot="header"><Header/></svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
 		<!-- Insert the list: -->
 		<nav class="list-nav">
---
+++ frontend/src/routes/components/header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { AppBar } from "@skeletonlabs/skeleton";
+    import SvelteLogo from '~icons/logos/svelte-icon'
+</script>
+
+<AppBar>
+	<svelte:fragment slot="lead">
+        <SvelteLogo/>
+    </svelte:fragment>
+	<h1 class="text-2xl">Derk Blom</h1>
+	<svelte:fragment slot="trail">(actions)</svelte:fragment>
+</AppBar>
