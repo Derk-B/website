10be5b8bd96f0ca14fa0f6d22b7655a1391ebae5
Derk Blom <74244634+Derk-B@users.noreply.github.com>
2023-10-18 23:37:04 +0200

    Added animation to error page

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -1,10 +1,28 @@
+<script>
+    import { fly } from 'svelte/transition';
+	import { cubicOut } from "svelte/easing";
+	import { onMount } from 'svelte';
+
+    let options = {duration: 1000, easing: cubicOut, y: -80};
+	let pageLoaded = false;
+
+	onMount(() => {
+		pageLoaded = true;
+	});
+</script>
+
 <div class="flex flex-col justify-center text-center h-full">
-    <div class="flex justify-center m-5">
-        <img width={100} src="404-error.png" alt="An icon indicating that this is a 404 error"/>
+    {#if pageLoaded}
+    <div in:fly={{...options}}>
+        <div class="flex justify-center m-5">
+            <img width={100} src="404-error.png" alt="An icon indicating that this is a 404 error"/>
+        </div>
+        <h1 class="text-4xl font-bold mb-3 bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone">Error</h1>
+        <h2 class="text-4xl mb-3 bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone">page not found</h2>
     </div>
-    <h1 class="text-4xl font-bold mb-3 bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone">Error</h1>
-    <h2 class="text-4xl mb-3 bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone">page not found</h2>
+    {/if}
 </div>
+
 
 <div class="iconref__bar">
     <a href="https://www.flaticon.com/free-icons/webpage" title="webpage icons">Webpage icons created by Freepik - Flaticon</a>
