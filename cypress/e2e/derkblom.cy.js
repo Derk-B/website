1eab6af14fb7e33627d10b8ec959ef02efc78c61
Derk Blom <74244634+Derk-B@users.noreply.github.com>
2023-12-20 23:32:37 +0100

    Added e2e tests for projects page and home page

--- cypress/e2e/derkblom.cy.js
+++ cypress/e2e/derkblom.cy.js
@@ -2,13 +2,26 @@ describe('Home page', () => {
   it('tests the dark/light mode button', () => {
     cy.visit('http://localhost:5173/')
 
-    cy.wait(1000)
+    cy.wait(500)
 
     cy.get(".lightswitch-track").scrollIntoView()
     cy.get(".lightswitch-track").should('be.visible');
     cy.get(".lightswitch-track").trigger("click")
     cy.get("body").should("have.css", "background-color", "rgb(36, 44, 70)")
-    // div lightswitch-thumb
-    
+  })
+})
+
+describe('Projects page', () => {
+  it('tests if navigation to projects page works', () => {
+    cy.visit('http://localhost:5173')
+    cy.get('a[href="/projects"]').click()
+    cy.location().should((location) => {
+      expect(location.href).to.eq("http://localhost:5173/projects")
+    })
+  })
+
+  it('tests if ate least 1 project cards are displayed', () => {
+    cy.visit('http://localhost:5173/projects')
+    cy.get('main').find('a').should('have.length.at.least', 1)
   })
 })
