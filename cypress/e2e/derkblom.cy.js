d43cf2b02d51b1f6a4ea19a1ebee34e6e9a7d909
Derk Blom <74244634+Derk-B@users.noreply.github.com>
2023-12-21 23:28:31 +0100

    Added e2e tests for all pages

--- cypress/e2e/derkblom.cy.js
+++ cypress/e2e/derkblom.cy.js
@@ -24,4 +24,54 @@ describe('Projects page', () => {
     cy.visit('http://localhost:5173/projects')
     cy.get('main').find('a').should('have.length.at.least', 1)
   })
+
+  it('tests if the project card contains all necessary elements', () => {
+    cy.visit('http://localhost:5173/projects')
+    cy.get('main').find('a').each(($item, index, $itemList) => {
+      cy.wrap($item).find('img').should("exist")
+      cy.wrap($item).find('h2').should("exist")
+      cy.wrap($item).find('p').should("exist")
+    })
+  })
+})
+
+describe('Blog page', () => {
+  it('tests if navigation to blog page works', () => {
+    cy.visit('http://localhost:5173')
+    cy.get('a[href="/blog"]').click()
+    cy.location().should((location) => {
+      expect(location.href).to.eq("http://localhost:5173/blog")
+    })
+  })
+
+  it('tests if ate least 1 blog cards are displayed', () => {
+    cy.visit('http://localhost:5173/blog')
+    cy.get('main').find('a').should('have.length.at.least', 1)
+  })
+
+  it('tests if the project card contains all necessary elements', () => {
+    cy.visit('http://localhost:5173/blog')
+    cy.get('main').find('a').each(($item, index, $itemList) => {
+      cy.wrap($item).find('img').should("exist")
+      cy.wrap($item).find('h2').should("exist")
+      cy.wrap($item).find('p').should("exist")
+    })
+  })
+
+  it('tests if the user is sent to the correct blog page', () => {
+    cy.visit('http://localhost:5173/blog')
+    cy.get('main').find('a').first().click()
+
+    cy.location("href").should('include', "http://localhost:5173/blogpost")
+  })
+})
+
+describe('blogpost page', () => {
+  it("tests if the page contains an image", () => {
+    cy.visit('http://localhost:5173/blog')
+    cy.get('main').find('a').first().click()
+
+    cy.get('h1').should('exist')
+    cy.get('img').should('exist')
+  })
 })
